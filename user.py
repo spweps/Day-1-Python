61fb46302a5962bdb8686842a7fb4c8e87384cbb
SPARRYW <scott.westerbeck@gmail.com>
2021-09-27 14:21:38 -0400

    userupdated.py

--- user.py
+++ user.py
@@ -1,25 +1,32 @@
+
 class User:
-    bank_name = "First National Dojo"
-    def __init__(self , name, email_address):
+    bank_name = "First National Dojo"
+    def __init__(self, name, email_address):
         self.name = name
         self.email = email_address
         self.account_balance = 0
-guido = User("Guido van Rossum", "guido@python.com")
-monty = User("Monty Python", "monty@python.com")
-print(guido.name)
-print(monty.name)
-
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-        self.account_balance = 0
     def make_deposit(self, amount):
-    	self.account_balance += amount
+        self.account_balance += amount
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+    def make_loan(self, user, amount):
+        self.account_balance -= amount
+        user.account_balance += amount
+    def account_total(self):
+        print(self.account_balance)
+scott = User("Scott Parry Westerbeck", "scott.westerbeck@gmail.com")
+jessica = User("Jessica Beisler", "jessica.b@beislerimages.com")
+silvia = User("Silvia Patricia Mariescurrena", "silmpat81@gmail.com")
 
-guido.make_deposit(100)
-guido.make_deposit(200)
-monty.make_deposit(50)
-print(guido.account_balance)
-print(monty.account_balance)
+scott.make_deposit(25000)
+jessica.make_deposit(100000)
+silvia.make_deposit(1000000000)
+
+scott.account_total()
+jessica.account_total()
+silvia.account_total()
+
+scott.make_loan(jessica, 25000)
+scott.account_total()
+jessica.account_total()
 
