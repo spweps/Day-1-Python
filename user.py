80c3cb0a4c95f7deab3053dadea114077af0b0a6
SPARRYW <scott.westerbeck@gmail.com>
2021-09-27 21:09:32 -0400

    user

--- user.py
+++ user.py
@@ -1,23 +1,32 @@
 
+from bankaccount2 import BankAccount
+
+
 class User:
     bank_name = "First National Dojo"
     def __init__(self, name, email_address):
         self.name = name
         self.email = email_address
         self.account_balance = 0
+        self.account = BankAccount 
+
     def make_deposit(self, amount):
         self.account_balance += amount
         return self
+
     def make_withdrawal(self, amount):
         self.account_balance -= amount
         return self
+
     def make_loan(self, user, amount):
         self.account_balance -= amount
         user.account_balance += amount
         return self
+
     def account_total(self):
         print(self.account_balance)
         return self
+        
 scott = User("Scott Parry Westerbeck", "scott.westerbeck@gmail.com")
 jessica = User("Jessica Beisler", "jessica.b@beislerimages.com")
 silvia = User("Silvia Patricia Mariescurrena", "silmpat81@gmail.com")
