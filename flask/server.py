c2f675c26e5cbbb70d66cf4afa1f86a5bfd355bf
SPARRYW <scott.westerbeck@gmail.com>
2021-09-30 16:03:32 -0400

    passing, render template, routing, jinja

--- flask/server.py
+++ flask/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import facts
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,8 +9,8 @@ def welcome():
 
 @app.route("/monsters/<monster>")  #<monster> is indicated in the URL where you choose the monster--bootstrapping
 def monster_page(monster):
-    print(monster)
-    return render_template("monster.html")
+    if monster = "dracula":
+        return render_template("monster.html", monster_name=monster.capitalize(), facts = facts.dracula_facts) #facts = f"{facts.monster}_facts"
 
 if __name__ == "__main__":
     app.run(debug=True)
