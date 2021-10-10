6e84880bfbc412454ddc3ca7e052fca3f4f8bd77
SPARRYW <scott.westerbeck@gmail.com>
2021-10-10 19:45:09 -0400

    user_cr updated code with corrections

--- users_assignment(s)/flask_app/controllers/server.py
+++ users_assignment(s)/flask_app/controllers/server.py
@@ -1,5 +1,29 @@
-from flask_app import app
-from flask_app.controllers import "class"
+from flask import Flask, render_template, request, redirect
 
-if __name__ =="__main__"
+from users import User
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/users')
+
+
+@app.route('/users')
+def users():
+    return render_template("users.html",users=User.get_all())
+
+
+@app.route('/user/new')
+def new():
+    return render_template("new_user.html")
+
+@app.route('/user/create',methods=['POST'])
+def create():
+    print(request.form)
+    User.save(request.form)
+    return redirect('/users')
+
+
+if __name__=="__main__":
     app.run(debug=True)
